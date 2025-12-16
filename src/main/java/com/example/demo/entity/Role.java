775f92c1f034dcbfe2598dd22d9006a8d292dfeb
bala-amypo <bala@amypo.in>
2025-12-16 14:35:11 +0000

    User commit

--- src/main/java/com/example/demo/entity/Role.java
+++ src/main/java/com/example/demo/entity/Role.java
@@ -1,28 +1,39 @@
 package com.example.demo.entity;
 
-public class Role {
-    private long id;
-    private String name;
-    public Role(){
+import jakarta.persistence.*;
 
-    }
-    public Role(String name) {
+@Entity
+public class Role {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+
+    public Role() {}
+
+    public Role(Long id, String name) {
+        this.id = id;
         this.name = name;
     }
-    public long getId() {
+
+
+    public Long getId() {
         return id;
     }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
-    public void setId(long id) {
-        this.id = id;
-    }
+
     public void setName(String name) {
         this.name = name;
     }
-    
-    
-    
-    
 }
