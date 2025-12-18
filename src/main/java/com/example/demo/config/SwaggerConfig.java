b56f355295bd597c9fb0db2966cad632bff57c88
bala-amypo <bala@amypo.in>
2025-12-18 15:47:35 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,45 +1,6 @@
-package com.example.demo.config;
-
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
-
-import org.springframework.context.annotation.Bean;
+package com.example.demo.config; 
+import io.swagger.v3.oas.models.OpenAPI; 
+import io.swagger.v3.oas.models.servers.Server; 
+import org.springframework.context.annotation.Bean; 
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
-
-@Configuration
-public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-
-        
-        SecurityScheme jwtScheme = new SecurityScheme()
-                .name("Authorization")
-                .type(SecurityScheme.Type.HTTP)
-                .scheme("bearer")
-                .bearerFormat("JWT")
-                .in(SecurityScheme.In.HEADER);
-
-        return new OpenAPI()
-                .info(new Info()
-                        .title("Asset Lifecycle Management API")
-                        .description("APIs for Asset, Vendor, Depreciation, Lifecycle Events, Disposal & Auth")
-                        .version("1.0.0")
-                )
-                
-                .servers(List.of(
-                        new Server().url("https://9126.408procr.amypo.ai")
-                ))
-                
-                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
-                .components(new Components()
-                        .addSecuritySchemes("BearerAuth", jwtScheme)
-                );
-    }
-}
+ import java.util.List; @Configuration public class SwaggerConfig { @Bean public OpenAPI customOpenAPI() { return new OpenAPI() // You need to change the port as per your server .servers(List.of( new Server().url("https://9126.408procr.amypo.ai") )); } }.give the code
