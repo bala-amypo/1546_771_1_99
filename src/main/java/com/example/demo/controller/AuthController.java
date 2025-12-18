c384f6430457fd753ad70cb289cff35a928a15c3
bala-amypo <bala@amypo.in>
2025-12-18 07:39:27 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -22,6 +22,6 @@ public class AuthController {
         if (user == null) {
             return "User not found!";
         }
-        return "Welcome " + user.getName();
+        return "Welcome " + user.getName(); // Now getName() exists
     }
 }
