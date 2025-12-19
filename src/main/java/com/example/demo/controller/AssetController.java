a51a84e0b31b30e2fa50c57d4ecc69f52ff99cdf
bala-amypo <bala@amypo.in>
2025-12-19 09:49:24 +0000

    User commit

--- src/main/java/com/example/demo/controller/AssetController.java
+++ src/main/java/com/example/demo/controller/AssetController.java
@@ -2,12 +2,15 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Asset;
 import com.example.demo.service.AssetService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/assets")
+@CrossOrigin
 public class AssetController {
 
     private final AssetService service;
@@ -18,11 +21,15 @@ public class AssetController {
 
     // POST /assets/vendor/{vendorId}/rule/{ruleId}
     @PostMapping("/vendor/{vendorId}/rule/{ruleId}")
-    public Asset createAsset(
+    public ResponseEntity<Asset> createAsset(
             @PathVariable Long vendorId,
             @PathVariable Long ruleId,
             @RequestBody Asset asset) {
-        return service.createAsset(vendorId, ruleId, asset);
+
+        Asset savedAsset = service.createAsset(vendorId, ruleId, asset);
+
+        // ✅ 201 CREATED
+        return new ResponseEntity<>(savedAsset, HttpStatus.CREATED);
     }
 
     @GetMapping
