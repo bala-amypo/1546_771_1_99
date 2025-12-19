22475c421f22654c5c34869c455754fd2d4fa10c
bala-amypo <bala@amypo.in>
2025-12-19 06:16:25 +0000

    User commit

--- src/main/java/com/example/demo/service/AssetDisposalService.java
+++ src/main/java/com/example/demo/service/AssetDisposalService.java
@@ -1,11 +1,25 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.AssetDisposal;
+import com.example.demo.repository.AssetDisposalRepository;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
-public interface AssetDisposalService {
+@Service
+public class AssetDisposalService {
 
-    AssetDisposal requestDisposal(AssetDisposal disposal);
+    private final AssetDisposalRepository repository;
 
-    List<AssetDisposal> getAllDisposals();
+    public AssetDisposalService(AssetDisposalRepository repository) {
+        this.repository = repository;
+    }
+
+    public AssetDisposal requestDisposal(AssetDisposal disposal) {
+        return repository.save(disposal); // 🔴 THIS INSERTS DATA
+    }
+
+    public List<AssetDisposal> getAllDisposals() {
+        return repository.findAll();
+    }
 }
