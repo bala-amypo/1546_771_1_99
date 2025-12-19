c2dd9108d7cde47e8adbdb283bc1b777460cdb96
bala-amypo <bala@amypo.in>
2025-12-19 06:22:25 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/AssetDisposalServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/AssetDisposalServiceImpl.java
@@ -1,24 +1,27 @@
-package com.example.demo.service;
+package com.example.demo.service.Impl;
 
 import com.example.demo.entity.AssetDisposal;
 import com.example.demo.repository.AssetDisposalRepository;
+import com.example.demo.service.AssetDisposalService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class AssetDisposalService {
+public class AssetDisposalServiceImpl implements AssetDisposalService {
 
     private final AssetDisposalRepository repository;
 
-    public AssetDisposalService(AssetDisposalRepository repository) {
+    public AssetDisposalServiceImpl(AssetDisposalRepository repository) {
         this.repository = repository;
     }
 
+    @Override
     public AssetDisposal requestDisposal(AssetDisposal disposal) {
-        return repository.save(disposal); // 🔴 THIS INSERTS DATA
+        return repository.save(disposal);
     }
 
+    @Override
     public List<AssetDisposal> getAllDisposals() {
         return repository.findAll();
     }
