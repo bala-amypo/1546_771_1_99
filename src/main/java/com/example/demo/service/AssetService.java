9e259fcbcdd0fc8242ae3f77980c2660850f9475
bala-amypo <bala@amypo.in>
2025-12-20 04:26:22 +0000

    User commit

--- src/main/java/com/example/demo/service/AssetService.java
+++ src/main/java/com/example/demo/service/AssetService.java
@@ -1,7 +1,6 @@
 package com.example.demo.service;
 
 import com.example.demo.entity.Asset;
-
 import java.util.List;
 
 public interface AssetService {
@@ -9,4 +8,8 @@ public interface AssetService {
     Asset createAsset(Long vendorId, Long ruleId, Asset asset);
 
     List<Asset> getAllAssets();
+
+    Asset getAsset(Long id);
+
+    List<Asset> getAssetsByStatus(String status);
 }
--- src/main/java/com/example/demo/service/Impl/AssetServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/AssetServiceImpl.java
@@ -1,7 +1,11 @@
-package com.example.demo.service.impl;
+package com.example.demo.service.Impl;
 
 import com.example.demo.entity.Asset;
+import com.example.demo.entity.DepreciationRule;
+import com.example.demo.entity.Vendor;
 import com.example.demo.repository.AssetRepository;
+import com.example.demo.repository.DepreciationRuleRepository;
+import com.example.demo.repository.VendorRepository;
 import com.example.demo.service.AssetService;
 import org.springframework.stereotype.Service;
 
@@ -10,21 +14,41 @@ import java.util.List;
 @Service
 public class AssetServiceImpl implements AssetService {
 
-    private final AssetRepository repository;
+    private final AssetRepository assetRepository;
+    private final VendorRepository vendorRepository;
+    private final DepreciationRuleRepository ruleRepository;
 
-    public AssetServiceImpl(AssetRepository repository) {
-        this.repository = repository;
+    public AssetServiceImpl(AssetRepository assetRepository,
+                            VendorRepository vendorRepository,
+                            DepreciationRuleRepository ruleRepository) {
+        this.assetRepository = assetRepository;
+        this.vendorRepository = vendorRepository;
+        this.ruleRepository = ruleRepository;
     }
 
     @Override
     public Asset createAsset(Long vendorId, Long ruleId, Asset asset) {
-        asset.setVendorId(vendorId);
-        asset.setRuleId(ruleId);
-        return repository.save(asset); // INSERTS INTO asset TABLE
+        Vendor vendor = vendorRepository.findById(vendorId).orElseThrow();
+        DepreciationRule rule = ruleRepository.findById(ruleId).orElseThrow();
+
+        asset.setVendor(vendor);
+        asset.setDepreciationRule(rule);
+
+        return assetRepository.save(asset);
     }
 
     @Override
     public List<Asset> getAllAssets() {
-        return repository.findAll();
+        return assetRepository.findAll();
+    }
+
+    @Override
+    public Asset getAsset(Long id) {
+        return assetRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public List<Asset> getAssetsByStatus(String status) {
+        return assetRepository.findByStatus(status);
     }
 }
