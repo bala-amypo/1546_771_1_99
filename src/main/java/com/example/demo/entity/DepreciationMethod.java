e4ad9535495d55a6cf88773e63e18a1ae846908f
bala-amypo <bala@amypo.in>
2025-12-25 10:08:09 +0000

    User commit

---
+++ src/main/java/com/example/demo/entity/DepreciationMethod.java
@@ -0,0 +1,5 @@
+package com.example.demo.entity;
+
+public enum DepreciationMethod {
+    STRAIGHT_LINE, DECLINING_BALANCE
+}
---
+++ src/main/java/com/example/demo/entity/EventType.java
@@ -0,0 +1,5 @@
+package com.example.demo.entity;
+
+public enum EventType {
+    AUDIT, REPAIR, MAINTENANCE, INSPECTION
+}
