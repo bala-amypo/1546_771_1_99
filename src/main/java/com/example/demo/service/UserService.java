ae6d19fc233e4545fa7188e013833d4cc861b32e
bala-amypo <bala@amypo.in>
2025-12-27 05:12:08 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.service;
-import com.example.demo.entity.User;
-import java.util.Map;
-public interface UserService {
-    User registerUser(Map<String, String> userData);
-}
