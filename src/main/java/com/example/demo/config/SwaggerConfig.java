482f31a5b5830e83cf9b98c4d527cba0a2f75722
bala-amypo <bala@amypo.in>
2025-12-28 15:47:46 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,22 +1,9 @@
-// package com.example.demo.config;
-// import io.swagger.v3.oas.models.OpenAPI;
-// import io.swagger.v3.oas.models.servers.Server;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import java.util.List;
-
-// @Configuration
-// public class SwaggerConfig {
-//     @Bean
-//     public OpenAPI customOpenAPI() {
-//         return new OpenAPI().servers(List.of(new Server().url("https://9070.408procr.amypo.ai/")));
-//     }
-// }
-
-
 package com.example.demo.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,8 +16,25 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-            .servers(List.of(
-                new Server().url("https://9070.pro604cr.amypo.ai/")
-            ));
+
+                // API Info
+                .info(new Info()
+                        .title("JWT Demo API")
+                        .version("1.0")
+                        .description("Simple JWT "))
+
+                // Server Configuration
+                .servers(List.of(
+                        new Server().url("https://9070.pro604cr.amypo.ai")
+                ))
+
+                // Security Configuration (JWT Bearer)
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Enter JWT token")));
     }
 }
